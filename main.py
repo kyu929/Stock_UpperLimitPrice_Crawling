ecd9f3abb1a4d53c54c51796375d12339ded0480
kyu929 <j2mity@gmail.com>
2021-03-01 23:12:39 +0900

    update main.py

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 stock_page = requests.get("https://finance.naver.com/sise/")
-print(stock_page.text)
+stock_page_html = stock_page.text
+
+soup = BeautifulSoup(stock_page_html, 'html.parser')
+print(soup.find_all(attrs={'summary': '탑종목 상한가 리스트'}))
