484f55d3f0b82fdf61e8ceecb606d465b55c7a50
kyu929 <j2mity@gmail.com>
2021-03-05 00:18:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import requests
+import os
+from selenium import webdriver
 from bs4 import BeautifulSoup
 
-stock_page = requests.get("https://finance.naver.com/sise/")
-stock_page_html = stock_page.text
+driver = webdriver.Chrome(
+    'C:/Users/User/Documents/Git/Stock_UpperLimitPrice_Crawling/chromedriver.exe'
+)
 
+driver.implicitly_wait(3)
+
+driver.get('https://finance.naver.com/sise/')
+
+stock_page_html = driver.page_source
 soup = BeautifulSoup(stock_page_html, 'html.parser')
-print(soup.select('td > a'))
+
+print(soup.select('#siselist_tab_0 > tbody > tr:nth-child(3) > td:nth-child(4)'))
