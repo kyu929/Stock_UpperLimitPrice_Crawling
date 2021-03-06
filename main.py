5fdb8c8bffa95c3e8e7ff7bc92c731032e6466e3
kyu929 <j2mity@gmail.com>
2021-03-06 23:59:35 +0900

    update main.py

--- main.py
+++ main.py
@@ -4,14 +4,23 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome(
-    'C:/Users/User/Documents/Git/Stock_UpperLimitPrice_Crawling/chromedriver.exe'
+    "./chromedriver"
 )
 
 driver.implicitly_wait(3)
 
-driver.get('https://finance.naver.com/sise/')
+naver_finance_url = 'https://finance.naver.com/sise/'
+naver_finance_market_url = 'https://finance.naver.com/sise/sise_market_sum.nhn?page=1'
+
+driver.get(naver_finance_url)
 
 stock_page_html = driver.page_source
-soup = BeautifulSoup(stock_page_html, 'html.parser')
 
-print(soup.select('#siselist_tab_0 > tbody > tr:nth-child(3) > td:nth-child(4)'))
+soup = BeautifulSoup(stock_page_html, 'html.parser')
+print(soup.select('#siselist_tab_0 > tbody'))
+print(soup.find("div", attrs={"class": "box_type_l"}).find("table").find_all("a", attrs={"class": "tltle"}))
+
+UpperLimitPrice_list = soup.find("div", attrs={"class": "box_type_l"}).find("table").find_all("a", attrs={"class": "tltle"})
+
+for ULP in UpperLimitPrice_list:
+    print(ULP.get_text().split())
